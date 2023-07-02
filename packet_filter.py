5f43d9516c11b9e7786dae9a21a374b1051e85de
Nitin Gupta <nitin.gupta.22@ucl.ac.uk>
2023-07-02 20:58:00 +0100

    updates to readability

--- packet_filter.py
+++ packet_filter.py
@@ -1,18 +1,65 @@
+import logging
 import os
 
 from scapy.all import rdpcap, wrpcap, Raw
+from scapy.layers.dns import DNS
+from scapy.layers.http import HTTP
 from scapy.layers.inet import TCP
+from scapy.packet import Padding
 from termcolor import colored
-import logging
+
 logging.getLogger('scapy.runtime').setLevel(logging.ERROR)
 
 # Define the packet filters
 FILTERS = {
-    "layers": ["ARP", "ICMP", "ICMPv6MLReport", "IPv6ExtHdrHopByHop", "ICMPv6ND_NS", "IPv6", "LLMNRQuery", "DNS",
-               "IGMP", "ICMPv6ND_RS", "igmp"],
-    "tcp_flags": ["S", "SA", "A", "R", "RA"],
+    "layers": ["ARP", "ICMP", "ICMPv6MLReport", "IPv6ExtHdrHopByHop", "ICMPv6ND_NS", "IPv6", "LLMNRQuery",
+               "IGMP", "ICMPv6ND_RS", "igmp", "DHCP6_Advertise", "DHCP6_Request"],
+    "tcp_flags": [],
     "extras": []
 }
+
+
+def get_readable_payload(packet):
+    """
+    This function tries to decode the packet payload and checks for potential signs of malicious activity.
+    """
+    payload_info = ""
+
+    # If the packet has Raw layer (meaning it has payload)
+    if packet.haslayer(Raw):
+        try:
+            # Try to decode the payload
+            payload = bytes(packet[Raw].load).decode('UTF8', 'replace')
+            payload_info += "\nRaw Layer Payload: " + payload
+        except:
+            payload_info += "\nRaw Layer Payload: " + repr(packet[Raw].load)
+
+    # If the packet has Padding layer
+    elif packet.haslayer(Padding):
+        try:
+            # Try to decode the padding
+            padding = bytes(packet[Padding].load).decode('UTF8', 'replace')
+            payload_info += "\nPadding Layer Payload: " + padding
+        except:
+            payload_info += "\nPadding Layer Payload: " + repr(packet[Padding].load)
+
+    # If the packet has TCP layer
+    elif packet.haslayer(TCP):
+        src_port = packet[TCP].sport
+        dst_port = packet[TCP].dport
+        payload_info += "\nTCP Layer: Src Port = " + str(src_port) + ", Dst Port = " + str(dst_port)
+
+    # If the packet has HTTP layer
+    elif packet.haslayer(HTTP):
+        http_info = packet[HTTP].fields
+        payload_info += "\nHTTP Layer: " + str(http_info)
+
+    # If the packet has DNS layer
+    elif packet.haslayer(DNS):
+        dns_info = packet[DNS].fields
+        payload_info += "\nDNS Layer: " + str(dns_info)
+
+    return payload_info
 
 
 def packet_filter(packet, filters):
@@ -29,10 +76,6 @@ def packet_filter(packet, filters):
         for flag in filters['tcp_flags']:
             if flag in packet[TCP].sprintf('%TCP.flags%'):
                 return False
-
-    # Check if packet has payload
-    if not packet.haslayer(Raw):
-        return False
 
     summary = packet.summary().lower()
     if any(word in summary for word in filters['extras']):
@@ -52,15 +95,15 @@ def sanitize_pcap(input_file, output_folder='sanitised_inputs', filters={}):
     # List to store the packets to keep
     packets_to_keep = []
 
-    for packet in packets:
+    for i, packet in enumerate(packets, 1):
         # Filter the packet
         if not packet_filter(packet, filters):
             continue
         summary = packet.summary()
 
         # Print a summary of the packet
-        print(colored(summary, 'green'))
-        print(colored("Payload: " + summary, 'green'))
+        print(colored(f"Packet No: `{i}`" + summary, 'magenta'))
+        print(colored("Payload: " + get_readable_payload(packet), 'green'))
 
         # Ask the user whether to keep the packet
         keep = input(
@@ -70,6 +113,8 @@ def sanitize_pcap(input_file, output_folder='sanitised_inputs', filters={}):
         if keep.lower() in ('yes', 'y'):
             packets_to_keep.append(packet)
             print(colored("Packet stored", 'blue'))
+        else:
+            os.system('cls' if os.name == 'nt' else 'clear')
         print('\n\n')
 
     # Ensure the output folder exists
@@ -77,15 +122,17 @@ def sanitize_pcap(input_file, output_folder='sanitised_inputs', filters={}):
 
     # Write the packets to keep to a new pcap file in the output folder
     output_file = os.path.join(output_folder, os.path.basename(input_file))
-    if len(packets_to_keep)>0:
+    if len(packets_to_keep) > 0:
         wrpcap(output_file, packets_to_keep)
     print(colored(f"Processed {input_file}\n\n\n", 'blue'))
 
 
-def process_files(directory, file_extension='.pcap', filters={}):
+def process_files(directory, file_extension='.pcap', filters=None):
     """
     This function processes all pcap files in the specified directory.
     """
+    if filters is None:
+        filters = {}
     try:
         for root, dirs, files in os.walk(directory):
             for file_name in files:
@@ -97,4 +144,4 @@ def process_files(directory, file_extension='.pcap', filters={}):
 
 
 # Call the process_files function to start processing pcap files in the 'inputs' directory
-process_files('./inputs', filters= FILTERS)
+process_files('./inputs', filters=FILTERS)
